74ed15e90c0f5485abcd37cfa8ea30c744ccb3c4
Quentin Mennecart <quentin.mennecart@gmail.com>
2025-01-13 09:55:15 +0100

    feat: add map component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,26 @@
 import "./App.css"
+import WorldMap from "./components/WorldMap"
 
 function App() {
-	return <>New App</>
+	return (
+		<div
+			style={{
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+				height: "100vh",
+			}}
+		>
+			<div
+				style={{
+					height: "90vh",
+					width: "90vw",
+				}}
+			>
+				<WorldMap />
+			</div>
+		</div>
+	)
 }
 
 export default App
